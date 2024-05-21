5a63730b3169349fc00a65fccd3c4a78166d4b09
SMAnish-28 <anish.coder.genius@gmail.com>
2024-05-22 01:04:24 +0530

    update dataRange

---
+++ DataRange.java
@@ -0,0 +1,35 @@
+import java.lang.*;
+
+class DataRange
+{
+    public static void main(String[] arg)
+    {
+        System.out.println("Byte Min: " + Byte.MIN_VALUE);
+        System.out.println("Byte Max: " + Byte.MAX_VALUE);
+        System.out.println("Byte Bytes: " + Byte.BYTES);
+
+        System.out.println("Short Min: " + Short.MIN_VALUE);
+        System.out.println("Short Max: " + Short.MAX_VALUE);
+        System.out.println("Short Bytes: " + Short.BYTES);
+
+        System.out.println("Long Min: " + Long.MIN_VALUE);
+        System.out.println("Long Max: " + Long.MAX_VALUE);
+        System.out.println("Long Bytes: " + Long.BYTES);
+
+        System.out.println("Int Min: " + Integer.MIN_VALUE);
+        System.out.println("Int Max: " + Integer.MAX_VALUE);
+        System.out.println("Int Bytes: " + Integer.BYTES);
+
+        System.out.println("Float Min: " + Float.MIN_VALUE);
+        System.out.println("Float Max: " + Float.MAX_VALUE);
+        System.out.println("Float Bytes: " + Float.BYTES);
+
+        System.out.println("Double Min: " + Double.MIN_VALUE);
+        System.out.println("Double Max: " + Double.MAX_VALUE);
+        System.out.println("Double Bytes: " + Double.BYTES);
+
+        System.out.println("Character Min: " + Character.MIN_VALUE);
+        System.out.println("Character Max: " + Character.MAX_VALUE);
+        System.out.println("Character Bytes: " + Character.BYTES);
+    }
+}
